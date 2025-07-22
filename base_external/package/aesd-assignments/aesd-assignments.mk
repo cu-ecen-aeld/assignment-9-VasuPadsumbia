d9c31bc0cf0e562af910ce5c05eff386677639f4
VasuPadsumbia <vasu.padsumbia@gmail.com>
2025-07-23 01:07:03 +0200

    Assignment 9 - AESD assignments complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 54b917fee2ba9590562d59f0db6faaf081d7580a
+AESD_ASSIGNMENTS_VERSION = 38ae6fc759fe5a1ef663d02fc3174a9e04d347f5
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -19,7 +19,7 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/aesd-char-driver KERNELDIR=$(LINUX_DIR) ARCH=arm64 CROSS_COMPILE=$(TARGET_CROSS) modules
 	
 
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server CFLAGS="$(TARGET_CFLAGS) -I$(@D)/aesd-char-driver" all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
@@ -37,6 +37,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	for f in $(@D)/aesd-char-driver/*; do \
 	    $(INSTALL) -m 0755 $$f $(TARGET_DIR)/etc/modules/aesdchar/; \
 	done
+	mkdir -p $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra
 	$(INSTALL) -m 0644 $(@D)/aesd-char-driver/aesdchar.ko \
         $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/
     $(HOST_DIR)/sbin/depmod -b $(TARGET_DIR) $(LINUX_VERSION_PROBED)
